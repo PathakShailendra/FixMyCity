96f2929b5f60073716eb58bf8be8606a011ece76
Shailendra Pathak <153452113+PathakShailendra@users.noreply.github.com>
2025-05-03 13:54:08 +0530

    Update Axios.js

--- frontend/src/utils/Axios.js
+++ frontend/src/utils/Axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Token fetcher (assuming it's stored in localStorage after login)
 const getToken = () => {
-  return localStorage.getItem("adminToken");
+  return localStorage.getItem("token");
 };
 
 const Axios = axios.create({
