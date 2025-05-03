f1f55c1efae11e0b219e7dc332fe7ea2aca0db5f
Shailendra Pathak <153452113+PathakShailendra@users.noreply.github.com>
2025-05-03 13:57:48 +0530

    Update isAdmin.js

--- backend/src/middlewares/isAdmin.js
+++ backend/src/middlewares/isAdmin.js
@@ -4,7 +4,7 @@ import Admin from '../models/admin.model.js'; // adjust path as needed
 
 export const isAdmin = async (req, res, next) => {
   try {
-    const token = req.cookies.token;
+    const token = req.cookies.token || request?.headers?.authorization?.split(" ")[1];
 
     if (!token) {
       return res.status(401).json({ message: 'Unauthorized: No token provided' });
